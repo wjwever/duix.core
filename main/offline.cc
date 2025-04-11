2ab8a8927792518b317201de80f934b47f2d9fb9
wjwever <1216451203@qq.com>
2025-04-11 17:59:35 +0800

    add offline

---
+++ main/offline.cc
@@ -0,0 +1,35 @@
+/*************************************************************************
+    > File Name: main.cpp
+    > Author: wangjiawei
+    > Mail: 1216451203@qq.com
+    > Created Time: 2025年03月12日 星期三 20时59分11秒
+ ************************************************************************/
+
+#include "clog.h"
+#include <edge_render.h>
+#include <getopt.hpp>
+#include <memory>
+#include <string>
+using namespace std;
+
+int main() {
+  std::string wav = getarg("", "-w", "--wav");
+  std::string role = getarg("siyao", "-r", "--role");
+  std::string baseDir = "gj_dh_res";
+
+  auto render = std::make_shared<EdgeRender>();
+  PLOGD << "load " << baseDir << " role:" << role;
+  render->load(baseDir, role);
+  std::thread th(&EdgeRender::render, render.get(), wav);
+  std::string data;
+  while (render and render->done() == false) {
+    render->getMsg(data);
+    if (data.size() > 0) {
+      PLOGD << data;
+    }
+  }
+
+  th.join();
+
+  return 0;
+}

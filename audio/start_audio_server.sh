14ddbf01b05935a8fc8188c15803be7a1070cd67
wjwever <1216451203@qq.com>
2025-04-24 08:48:26 +0800

    update script

--- audio/start_audio_server.sh
+++ audio/start_audio_server.sh
@@ -1,7 +1,7 @@
 dir=`dirname $0`
 cd $dir
 sudo apt update
-sudo apt install python3  python3-pip ffmpeg
+sudo apt install python3  python3-pip ffmpeg aria2
 pip3 install -r requirements.txt   -i https://pypi.doubanio.com/simple/
 echo "download SenseVoiceSmall from huggingface"
 export HF_ENDPOINT="https://hf-mirror.com"
